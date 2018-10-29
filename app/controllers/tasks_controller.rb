ffcb4b26d0aec9b6057a380b0cb37d5798e47e94
bondar5471 <bondar54711745@gmail.com>
2018-10-29 15:22:58 +0200

    re

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,24 @@
 class TasksController < ApplicationController
-  before_action :find_day, only: [:create, :destroy]
+  before_action :find_day, only: [:destroy]
   respond_to :json
   def create
     byebug
-    @task = @day.tasks.create(task_params)
+    @day = Day.find(params[:day_id])
+    @day.tasks.create(task_params)
+    respond_to do |format|
+      format.json { render json: @day.tasks.last }
+    end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
   end
 
   private
 
   def find_day
-    @day = Day.find(params[:day_id])
-    
+    byebug
+    @task = Task.find(params[:id])
   end
 
   def task_params
